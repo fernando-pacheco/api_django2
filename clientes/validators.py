dce9b25ac9dbfd6d3dff672fc0cdfb8366b34e47
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2023-12-30 11:12:16 -0300

    adicionando script, paginação e filtros para a api

--- clientes/validators.py
+++ clientes/validators.py
@@ -1,8 +1,16 @@
-def validate_cpf(cpf):
-        return len(cpf) == 11
+import re
+from validate_docbr import CPF
+
+
+def validate_cpf(numero_cpf):
+        cpf = CPF()
+        return cpf.validate(numero_cpf)
     
 def validate_celular(celular):
-    return len(celular) < 11
+    '''Verifica se o celular é válido xx xxxxx-xxxx'''
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo,celular)
+    return resposta
     
 def validate_rg(rg):
     return len(rg) == 9

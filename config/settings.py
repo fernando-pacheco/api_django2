0c2292ebeaf0406804db5bcfdb579eedd003e26f
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2023-12-31 09:53:30 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '(_nz-7j=$7we1y6$8i%bftxt2l_udez1ou_x7@*_x8t+(0e2lk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
